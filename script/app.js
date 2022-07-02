6aa6b5a5e09fb50c2008687cd0cce062300e664a
Yousuf Islam Rathore <rathoreislam@gmail.com>
2022-07-02 20:10:59 +0500

    Updated

--- script/app.js
+++ script/app.js
@@ -8,3 +8,22 @@ const showHideFunction = () => {
         loginPasswordInput.type = "password"
     }
 };
+
+const checkToggler = (box) => {
+    if(box.className == "fa-regular fa-square") {
+        box.className = "fa-solid fa-square-check"
+    }
+    else {
+        box.className = "fa-regular fa-square"
+    }
+
+}
+
+const starToggler = (e) => {
+    if(e.className == "fa-regular fa-star") {
+        e.className = "fa-solid fa-star"
+    }
+    else {
+        e.className = "fa-regular fa-star"
+    }
+}
